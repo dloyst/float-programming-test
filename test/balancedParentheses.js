505f56cf85449adb29158ce2bba1cd866f1b0a5b
Dave Loyst <david.loyst@gmail.com>
2016-07-07 14:12:49 -0700

    Added test for balancedParens function that tests for separate and nested parentheses as well as balanced brackets and balanced braces, but only if parentheses are present

--- test/balancedParentheses.js
+++ test/balancedParentheses.js
@@ -35,3 +35,75 @@ test( 'BALANCED PARENTHESES: returns false for string with no parentheses', t =>
     t.equal( result, false, 'result is correct' );
     t.end();
 } );
+
+test( 'BALANCED PARENTHESES: returns false for balanced brackets', t => {
+    const input = '[x + y]';
+    const result = fpt.balancedParentheses( input );
+
+    t.equal( typeof result, 'boolean', 'generated a boolean result' );
+    t.equal( result, false  , 'result is correct' );
+    t.end();
+} );
+
+test( 'BALANCED PARENTHESES: returns false balanced nested brackets', t => {
+    const input = '[[x] + [y]]';
+    const result = fpt.balancedParentheses( input );
+
+    t.equal( typeof result, 'boolean', 'generated a boolean result' );
+    t.equal( result, false, 'result is correct' );
+    t.end();
+} );
+
+test( 'BALANCED PARENTHESES: returns false for balanced braces', t => {
+    const input = '{x + y}';
+    const result = fpt.balancedParentheses( input );
+
+    t.equal( typeof result, 'boolean', 'generated a boolean result' );
+    t.equal( result, false, 'result is correct' );
+    t.end();
+} );
+
+test( 'BALANCED PARENTHESES: returns false balanced nested braces', t => {
+    const input = '{{x} + {y}}';
+    const result = fpt.balancedParentheses( input );
+
+    t.equal( typeof result, 'boolean', 'generated a boolean result' );
+    t.equal( result, false, 'result is correct' );
+    t.end();
+} );
+
+test( 'BALANCED PARENTHESES: returns true for separated balanced parens, braces, and brackets', t => {
+    const input = '(x + y){i + j}[k + z]';
+    const result = fpt.balancedParentheses( input );
+
+    t.equal( typeof result, 'boolean', 'generated a boolean result' );
+    t.equal( result, true, 'result is correct' );
+    t.end();
+} );
+
+test( 'BALANCED PARENTHESES: returns true for nested balanced parens, braces, and brackets', t => {
+    const input = '{[(x + y)]}';
+    const result = fpt.balancedParentheses( input );
+
+    t.equal( typeof result, 'boolean', 'generated a boolean result' );
+    t.equal( result, true, 'result is correct' );
+    t.end();
+} );
+
+test( 'BALANCED PARENTHESES: returns false for unbalanced separate parens, braces, and brackets', t => {
+    const input = '(x + y){i + j[k + z]';
+    const result = fpt.balancedParentheses( input );
+
+    t.equal( typeof result, 'boolean', 'generated a boolean result' );
+    t.equal( result, false, 'result is correct' );
+    t.end();
+} );
+
+test( 'BALANCED PARENTHESES: returns false for unbalanced nested parens, braces, and brackets', t => {
+    const input = '({[[x]} + [y]])';
+    const result = fpt.balancedParentheses( input );
+
+    t.equal( typeof result, 'boolean', 'generated a boolean result' );
+    t.equal( result, false, 'result is correct' );
+    t.end();
+} );
