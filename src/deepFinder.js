3bbbc5375edfbad512e6d2a96e7fed57172ca220
Dave Loyst <david.loyst@gmail.com>
2016-07-07 11:41:33 -0700

    completed deepFinder function that runs a callback on nested array elements and nested object enumerables

--- src/deepFinder.js
+++ src/deepFinder.js
@@ -15,4 +15,39 @@
  */
 module.exports = ( input, test ) => {
 
+  const truthyValues = [];
+	
+	if (!Array.isArray(input)) return truthyValues;
+	if (typeof test !== 'function') return truthyValues;
+	
+  findInArray(input, test);
+  
+  function findInArray(input, test) {
+    for (let i = 0; i < input.length; i++) {
+      testElement(input[i], test);
+    }
+  }
+
+  function findInObject(input, test) {
+    for (let key in input) {
+      testElement(input[key], test);
+    }
+  }
+  
+  function testElement(element, test) {
+
+  	//if element is an array, test each element in that array
+  	if (Array.isArray(element)) findInArray(element, test);
+
+  	//if element is instead an object, test each enumerable
+  	else typeof element === 'object' ?
+  		findInObject(element, test) :
+  		(
+  			
+  			//if element is neither an array nor object, run callback on element
+  			test(element) ?
+  			truthyValues.push(element) : null
+  		);
+  }
+  return truthyValues;
 };
