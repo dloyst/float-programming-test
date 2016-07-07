7b64b9857c68b6327f569dc4451defc3434fb72d
Dave Loyst <david.loyst@gmail.com>
2016-07-07 11:33:19 -0700

    added tests for finder function testing for proper argument values and that function does not test within nested arrays or objects

--- test/finder.js
+++ test/finder.js
@@ -17,5 +17,62 @@ test( 'FINDER: finds strings that start with a', t => {
 
     t.ok( result, 'generated a result' );
     t.deepEqual( result, [ 'ant', 'allegory' ], 'result is correct' );
+    t.notEqual( result, [ 'ant', 'apple', 'allegory' ], 'result does not include values from nested arrays' );
+    t.notEqual( result, [ 'ant', 'aardvark', 'allegory' ], 'result does not include values from nested objects' );
+    t.end();
+} );
+
+test( 'FINDER: finds even numbers', t => {
+    const input = [ 'apple', 5, 'bear', 10, ['cat', 12, 'deer', 7], {
+        odd: 9, 
+        even: 16, 
+        word: 'eel'
+    }, 19, 20, 'fall' ];
+    const result = fpt.finder( input, value => value % 2 === 0 );
+
+    t.ok( result, 'generated a result' );
+    t.deepEqual( result, [ 10, 20 ], 'result is correct' );
+    t.notEqual( result, [ 10, 12, 20 ], 'result does not include values from nested arrays' );
+    t.notEqual( result, [ 10, 16, 20 ], 'result does not include values from nested objects' );
+    t.end();
+} );
+
+test( 'FINDER: returns empty array when input is undefined', t => {
+    const input = [];
+    const result = fpt.finder( input, value => value % 2 === 0 );
+
+    t.ok( result, 'generated a result' );
+    t.deepEqual( result, [], 'result is correct' );
+    t.end();
+} );
+
+test( 'FINDER: returns empty array when input is null', t => {
+    const input = [];
+    const result = fpt.finder( input, value => value % 2 === 0 );
+
+    t.ok( result, 'generated a result' );
+    t.deepEqual( result, [], 'result is correct' );
+    t.end();
+} );
+
+test( 'FINDER: returns empty array when input is not an array', t => {
+    const input = 'hello world';
+    const result = fpt.finder( input, value => value % 2 === 0 );
+
+    t.ok( result, 'generated a result' );
+    t.deepEqual( result, [], 'result is correct' );
+    t.end();
+} );
+
+test( 'FINDER: returns empty array when test is not a function', t => {
+    const input = [ 'apple', 'bear', 10, ['cat', 'deer', 15], {
+        f: 'fish',
+        g: 'goat',
+        even: 16
+    }, 'horse', 20];
+    const result = fpt.finder( input, 'hello world' );
+
+    t.ok( result, 'generated a result' );
+    t.deepEqual( result, [], 'result is correct' );
     t.end();
 } );
